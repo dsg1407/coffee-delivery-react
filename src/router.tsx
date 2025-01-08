b7dd8ee41ca063f04842e92f7c88d7650de74d94
Daniel S Goncalves <dsg1407@gmail.com>
2025-01-08 18:40:13 -0300

    add order success route

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,9 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router'
 
 import { DefaultLayout } from './layouts/default-layout'
 import { Checkout } from './pages/checkout'
 import { Home } from './pages/home'
+import { Success } from './pages/order/success'
 
 export function Router() {
   return (
@@ -10,6 +11,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order/success/*" element={<Success />} />
       </Route>
     </Routes>
   )
